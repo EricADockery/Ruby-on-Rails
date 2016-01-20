dc14ab4c5dc0cea8abd9b3c8549765f0f50470d3
EricADockery <eadock01@louisville.edu>
2016-01-20 16:51:49 -0500

    Basic Ruby Programmin
    This is some of the Basics of the Ruby language from Treehouse.com

---
+++ Ruby Basics/hello.rb
@@ -0,0 +1,10 @@
+name = " Eric 
+This is a test
+for multiple output
+"
+puts "Hello\n#{name}!"
+
+year = 2016
+puts "The year is #{year}"
+future = 5
+puts "In 5 years, the year will be #{year+ future}"
---
+++ Ruby Basics/methods.rb
@@ -0,0 +1,10 @@
+#Method to add two numbers.
+def add(a, b)
+	puts"Adding #{a} and #{b}:"
+
+	return a+b
+
+end
+
+sum = add(3,5)
+puts sum
